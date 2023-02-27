dc27537f9dcc6a4d67a36466549b8ad727117ae3
Alofe Oluwafemi <oluwafemialofe@yahoo.com>
2023-02-28 00:36:25 +0400

    Upgrade membership to gold

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get(
     "/membership/0x2A6B7BA38BD8BED065C68361c383A77C160C5Fd7",
     (req, res) => {
         res.json({
-            level: 0, // 0 = silver, 1 = gold, 2 = platinum
+            level: 1, // 0 = silver, 1 = gold, 2 = platinum
         });
     }
 );
